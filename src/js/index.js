2cfef9f3ee1426f537f8e971a73cc6c57509935f
Iuliana <dolganova.yuliana@yandex.ru>
2023-05-22 10:17:52 +0300

    Finish layout, css  and js

--- src/js/index.js
+++ src/js/index.js
@@ -1,131 +1,69 @@
 import '../scss/style.scss'
+import swiperCreate from './functions/swiper'
+import showHideElements from './functions/showHideElements'
+import showModal from './functions/showModal'
+import hideModal from './functions/hideModal'
+import hideModalEsc from './functions/hideModalEsc'
+import closeClickBlur from './functions/closeClickBlur'
 
 if (document.documentElement.clientWidth < 768) {
-  document
-    .querySelectorAll(
-      '.brands__container, .services__container, .price__container'
-    )
-    .forEach((elem) => elem.classList.add('swiper'))
-  document
-    .querySelectorAll('.brands__list, .services__list, .price__list')
-    .forEach((elem) => elem.classList.add('swiper-wrapper'))
-
-  const swiperSlide = document.querySelectorAll(
-    '.brands__item, .services__item, .price__table-line'
-  )
-  for (let i = 0; i < swiperSlide.length; i++) {
-    swiperSlide[i].classList.add('swiper-slide')
-  }
-
-  const swiperBrands = new Swiper('.swiper-brands', {
-    sliderPerView: 'auto',
-    spaceBetween: 16,
-    mousewheel: true,
-    pagination: {
-      el: '.swiper-pagination-brands',
-      clickable: true
-    }
-  })
-  const swiperServices = new Swiper('.swiper-services', {
-    sliderPerView: 'auto',
-    spaceBetween: 16,
-    mousewheel: true,
-    pagination: {
-      el: '.swiper-pagination-services',
-      clickable: true
-    }
-  })
-
-  const swiperPrice = new Swiper('.swiper-price', {
-    sliderPerView: 'auto',
-    spaceBetween: 16,
-    mousewheel: true,
-    pagination: {
-      el: '.swiper-pagination-price',
-      clickable: true
-    }
-  })
+  swiperCreate()
 }
 
-const brandsButton = document.querySelector('.brands__button')
-brandsButton.addEventListener('click', () => {
-  changeVisibility('.brands__item')
-  changeButton(brandsButton, 'Показать все', 'Скрыть')
-})
+showHideElements(
+  '.services__button',
+  '.services__item',
+  'Показать все',
+  'Скрыть'
+)
 
-const aboutButton = document.querySelector('.about__button')
-aboutButton.addEventListener('click', () => {
-  changeVisibility('.about__text-item')
-  changeButton(aboutButton, 'Читать далее', 'Скрыть')
-})
+showHideElements('.brands__button', '.brands__item', 'Показать все', 'Скрыть')
 
-const servicesButton = document.querySelector('.services__button')
-servicesButton.addEventListener('click', () => {
-  changeVisibility('.services__item')
-  changeButton(servicesButton, 'Показать все', 'Скрыть')
-})
+showHideElements(
+  '.about__button',
+  '.about__text-item',
+  'Читать далее',
+  'Скрыть'
+)
 
-function changeButton(button, openText, closeText) {
-  button.classList.toggle('button-show')
-  if (button.classList.contains('button-show')) {
-    button.textContent = openText
-    return
-  } else {
-    button.textContent = closeText
-  }
-}
-
-function changeVisibility(block) {
-  let changeBlock = document.querySelectorAll(block)
-  for (let i = 0; i < changeBlock.length; i++) {
-    changeBlock[i].classList.toggle('open')
-  }
-}
-
-function openCloseModal(block) {
-  block.classList.toggle('open')
-  overlay.classList.toggle('overlay-active')
-}
-
+const feedback = document.querySelector('.feedback')
+const feedbackContainer = document.querySelector('.feedback-container')
+const feedbackOverlay = document.querySelector('.feedback-overlay')
+const feedbackButtonClose = document.querySelector('.feedback__button-close')
+const openMessageMenu = document.querySelector('.menu__button-message')
+const openMessageHeader = document.querySelector('.header__button-message')
 const menu = document.querySelector('.menu')
 const menuContainer = document.querySelector('.menu-container')
-const overlay = document.querySelector('.overlay')
-const menuButtonOpen = document.querySelector('.header__menu-button')
+const menuOverlay = document.querySelector('.menu-overlay')
 const menuButtonClose = document.querySelector('.menu__button-close')
-menuButtonOpen.addEventListener('click', () => {
-  openCloseModal(menu)
-})
-menuButtonClose.addEventListener('click', () => {
-  openCloseModal(menu)
-})
-// window.addEventListener('click', function (e) {
-//   if (menu.classList.contains('open') && !menuContainer.contains(e.target)) {
-//     // menu.classList.remove('open')
-//     menu.style.display = 'none'
-//   }
-// })
-// document.addEventListener('click', outsideEvtListener)
-// function outsideEvtListener(evt) {
-//   if (evt.target === menuContainer || menuContainer.contains(evt.target)) {
-//     return
-//   }
-//   menu.classList.add('open')
-// }
+const openMenu = document.querySelector('.header__menu-button')
+const call = document.querySelector('.call')
+const callContainer = document.querySelector('.call-container')
+const callOverlay = document.querySelector('.call-overlay')
+const callButtonClose = document.querySelector('.call__button-close')
+const openCallMenu = document.querySelector('.menu__button-call')
+const openCallHeader = document.querySelector('.header__button-call')
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape') {
-    menu.classList.toggle('open')
-    overlay.classList.toggle('overlay-active')
-  }
-})
+showModal(menu, menuOverlay, openMenu)
+showModal(feedback, feedbackOverlay, openMessageMenu)
+showModal(feedback, feedbackOverlay, openMessageHeader)
+showModal(call, callOverlay, openCallHeader)
+showModal(call, callOverlay, openCallMenu)
 
-document.addEventListener('click', function (e) {
-  if (
-    menu.classList.contains('open') &&
-    !menuContainer.contains(e.target) &&
-    !menuButtonOpen.contains(e.target)
-  ) {
-    menu.classList.toggle('open')
-    overlay.classList.toggle('overlay-active')
-  }
-})
+hideModal(menu, menuOverlay, menuButtonClose)
+hideModal(feedback, feedbackOverlay, feedbackButtonClose)
+hideModal(call, callOverlay, callButtonClose)
+
+hideModalEsc(menu, menuOverlay)
+hideModalEsc(feedback, feedbackOverlay)
+hideModalEsc(call, callOverlay)
+
+closeClickBlur(menu, menuContainer, menuOverlay, openMenu, openMenu)
+closeClickBlur(
+  feedback,
+  feedbackContainer,
+  feedbackOverlay,
+  openMessageMenu,
+  openMessageHeader
+)
+closeClickBlur(call, callContainer, callOverlay, openCallMenu, openCallHeader)
